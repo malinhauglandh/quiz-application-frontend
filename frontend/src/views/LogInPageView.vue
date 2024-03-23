4b56cc8445b32a089ffac8c01eff6634ea45bc1f
Ina <inamar@stud.ntnu.no>
2024-03-23 13:16:38 +0100

    Create a LogInPageView.vue for logging into the application.

---
+++ frontend/src/views/LogInPageView.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import LogInPage from "../components/LogInPage.vue";
+</script>
+
+<template>
+  <LogInPage />
+</template>
+
+<script>
+export default {
+  name: "LogInPage",
+};
+</script>
