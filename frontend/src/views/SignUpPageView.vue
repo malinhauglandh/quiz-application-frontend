1498b0fc3fc59780a1610b8e068e8fa6d80d558f
Ina <inamar@stud.ntnu.no>
2024-03-23 13:33:46 +0100

    Create SignUpPageView.vue to sign up a new user.

---
+++ frontend/src/views/SignUpPageView.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import SignUpPage from "../components/SignUpPage.vue";
+</script>
+
+<template>
+  <SignUpPage />
+</template>
+
+<script>
+export default {
+  name: "SignUpPage",
+};
+</script>
