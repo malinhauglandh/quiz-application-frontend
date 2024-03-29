a39dde6d45fcc09b9f20ccc4f7f3b50818083b56
Ina <inamar@stud.ntnu.no>
2024-03-29 17:33:40 +0100

    Change to composition API.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,17 +6,30 @@
   </body>
 </template>
 
-<script>
-export default {
-  name: 'App',
-}
+<script setup>
+import { defineComponent} from "vue";
+
+export default defineComponent({
+  name: "App",
+});
 </script>
 
 <style>
 body, html {
   margin: 0;
   padding: 0;
-  background-image: linear-gradient(120deg, #bcb6ff 20%, #6320EE 80%);
+  /*background-image: linear-gradient(120deg, #bcb6ff 20%, #6320EE 80%);*/
+  background-color: #BCB6FF;
+}
+
+body {
+    background-color: #BCB6FF;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
 }
 </style>
-```
