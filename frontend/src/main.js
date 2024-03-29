b9a3ceee7181df331a7031445a91bc05a7c481d5
Ina <inamar@stud.ntnu.no>
2024-03-29 17:35:11 +0100

    Update folder named router.js in main.js.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './routes/router'
+import router from './router/router.js'
 
 createApp(App).use(router).mount('#app')
