144be4cdb08616d9485abe146500de7ce4a61fd4
Eline Evje <elineev@stud.ntnu.no>
2024-03-30 15:46:24 +0100

    Added view for home and search page

---
+++ frontend/src/view/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-view">
+        <navbar-component></navbar-component>
+        <div class="content">
+            <HomeComponent />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import NavbarComponent from '@/components/NavBarComponent.vue';
+import HomeComponent from '@/components/HomeComponent.vue';
+
+</script>
+
+<style scoped>
+</style>
