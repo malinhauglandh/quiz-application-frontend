ba6a566f2fefafbf170faa34fe8555bd85c44040
Eline Evje <elineev@stud.ntnu.no>
2024-04-04 17:09:00 +0200

    Made components for the different question types for when playing a quiz

---
+++ frontend/src/components/FillInTheBlankQuestionComponent.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="fill-in-the-blank-question">
+        <div class="question-display">
+            <p class="question-text">What is the capital of Finland?</p>
+        </div>
+        <div class="input-container">
+            <input
+                type="text"
+                class="answer-input"
+                placeholder="Answer"
+                v-model="userAnswer"
+            />
+        </div>
+        <div class="submit-container">
+            <button class="button submit-button" @click="submitAnswer">SUBMIT ANSWER AND GO TO NEXT QUESTION</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const userAnswer = ref('');
+const router = useRouter();
+
+const submitAnswer = () => {
+    console.log('Submitted answer:', userAnswer.value);
+    router.push('/home');
+};
+
+</script>
+
+<style scoped>
+.question-display {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.question-text {
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.input-container {
+    display: flex;
+    justify-content: center;
+}
+
+.answer-input {
+    width: 250px;
+    padding: 10px;
+    margin-bottom: 30px;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 18px;
+    text-align: center;
+}
+
+.answer-input:focus {
+    outline: none;
+    border-color: #6320EE;
+    box-shadow: 0 0 5px #6320EE;
+}
+
+.submit-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+.button.submit-button {
+    padding: 15px 30px;
+    background-color: #6320EE;
+    color: white;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+.button.submit-button:hover {
+    background-color: #7E41FDFF;
+}
+</style>
---
+++ frontend/src/components/TrueOrFalseQuestionComponent.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="true-or-false-question">
+        <div class="question-display">
+            <p class="question-text">Copenhagen is the capital of Denmark.</p>
+        </div>
+        <div class="alternatives-container">
+            <button
+                v-for="(alternative, index) in alternatives"
+                :key="alternative"
+                class="alternative-button"
+                :class="{ 'selected': selectedAnswer === index }"
+                @click="selectAlternative(index)"
+            >
+                {{ alternative }}
+            </button>
+        </div>
+        <div class="submit-container">
+            <button class="button submit-button" @click="submitAnswer">SUBMIT ANSWER AND GO TO NEXT QUESTION</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const alternatives = ['True', 'False'];
+const selectedAnswer = ref(null);
+const router = useRouter();
+
+const selectAlternative = (n) => {
+    selectedAnswer.value = n;
+};
+
+const submitAnswer = () => {
+    console.log('Submitted alternative:', selectedAnswer.value);
+    router.push('/home');
+};
+
+
+</script>
+
+<style scoped>
+.question-display {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.question-text {
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.alternatives-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.alternative-button {
+    width: 250px;
+    height: 100px;
+    padding: 10px;
+    margin-bottom: 30px;
+    background-color: #f0f0f0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+    transition: background-color 0.3s ease;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+}
+
+.alternative-button:hover {
+    background-color: #e0e0e0;
+}
+
+.alternative-button.selected {
+    background-color: #f7567c;
+    color: white;
+}
+
+.submit-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+.button.submit-button {
+    padding: 15px 30px;
+    background-color: #6320EE;
+    color: white;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+.button.submit-button:hover {
+    background-color: #7E41FDFF;
+}
+</style>
